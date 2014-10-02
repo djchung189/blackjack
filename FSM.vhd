64e20df39c8549b65bd0a9859f92a166648d270e
njackien <deterex@gmail.com>
2014-10-01 17:04:56 -0700

    Update FSM.vhd

--- FSM.vhd
+++ FSM.vhd
@@ -7,7 +7,7 @@ entity FSM is
 	port(
 		clock : IN STD_LOGIC;
 		reset : IN STD_LOGIC;
-
+		
 		nextStep     : IN STD_LOGIC; -- when true, it advances game to next step
 		playerStands : IN STD_LOGIC; -- true if player wants to stand
 		dealerStands : IN STD_LOGIC; -- true if dealerScore >= 17
@@ -31,15 +31,16 @@ architecture BEHAVIOURAL of FSM is
 	signal PRESENT_STATE : state_type;
 	signal resetsignal : std_LOGIC;
 	signal nextsignal : std_LOGIC;
+	
+		
 begin
-	resetsignal <= reset;
-	nextsignal <= nextStep;
+	resetsignal<=reset;
+	nextsignal<=nextStep;
 	
 	process (clock ,resetsignal)
-		--variable PRESENT_STATE : state_type;
-		--variable PRESENT_STATE : bit_vector(2 downto 0) <= "000";
+	
 	begin
-		if(reset = '0') then
+		if(resetsignal = '0') then
 			PRESENT_STATE <= idle;
 		elsif(rising_edge(clock)) then
 			If(PRESENT_STATE=idle)then --deal card to player
@@ -48,21 +49,21 @@ begin
 				else
 					PRESENT_STATE <= idle;
 				end if;
-				
+				greenLEDs<="00000000";
 			elsif(PRESENT_STATE=player_1)then--deal card to dealer
 				if(nextsignal='0') then
 					PRESENT_STATE <= dealer_1;
 				else
 					PRESENT_STATE <= player_1;
 				end if;
-				
+				greenLEDs<="00000001";
 			elsif(PRESENT_STATE=dealer_1)then-- deal card to player and check if it should proceed to next stage
 				if(nextsignal='0') then
 					PRESENT_STATE <= player_2;
 				else
 					PRESENT_STATE <= dealer_1;
 				end if;
-				
+				greenLEDs<="00000010";
 			elsif(PRESENT_STATE=player_2)then
 				if(nextsignal='0' and playerStands='0') then
 					PRESENT_STATE <= player_3_hit;
@@ -71,7 +72,7 @@ begin
 				else
 					PRESENT_STATE <= player_2;
 				end if;
-			
+				greenLEDs<="00000011";
 			elsif(PRESENT_STATE=player_3_hit)then
 				if(nextsignal='0' and playerStands='0' and playerBust='0' ) then--
 					PRESENT_STATE <= player_4_hit;
@@ -139,41 +140,44 @@ begin
 				PRESENT_STATE <= end_game;
 			end IF;
 		end If;
+		
+		--
+		--
 	end process;
 	
-	process(PRESENT_STATE)
-	begin
-		If(PRESENT_STATE=idle)then
-			newPlayerCard <="0000";
-			newDealerCard <="0000";
-
-			redLEDs   <="000000000000000000";
-			greenLEDs <="00000000";
-		elsif(PRESENT_STATE=player_1)then 
-			newPlayerCard <= "0001";
-		elsif(PRESENT_STATE=dealer_1)then 
-			newDealerCard <= "0001";
-		elsif(PRESENT_STATE=player_2)then 
-			newPlayerCard <= "0011";
-		elsif(PRESENT_STATE=player_3_hit)then 
-			newPlayerCard <= "0111";
-		elsif(PRESENT_STATE=player_4_hit)then 
-			newPlayerCard <= "1111";
-		elsif(PRESENT_STATE=dealer_2)then
-			newDealerCard <= "0011";
-		elsif(PRESENT_STATE=dealer_3_hit)then
-			newDealerCard <= "0111";
-		elsif(PRESENT_STATE=dealer_4_hit)then
-			newDealerCard <= "1111";
-		elsif(PRESENT_STATE=player_win)then 
-			greenLEDs<="11111111";
-		elsif(PRESENT_STATE=both_bust)then
-			greenLEDs<="11111111";
-			redLeds<="111111111111111111";
-		elsif(PRESENT_STATE=dealer_win)then 
-			redLeds<="111111111111111111";
-		elsif(PRESENT_STATE=end_game)then 
-		else		
-		end if;
-	end process;
+--	process(PRESENT_STATE)
+--	begin
+--		If(PRESENT_STATE=idle)then
+--			newPlayerCard <="0000";
+--			newDealerCard <="0000";
+--
+--			redLEDs   <="000000000000000000";
+--			greenLEDs <="00000000";
+--		elsif(PRESENT_STATE=player_1)then 
+--			newPlayerCard <= "0001";
+--		elsif(PRESENT_STATE=dealer_1)then 
+--			newDealerCard <= "0001";
+--		elsif(PRESENT_STATE=player_2)then 
+--			newPlayerCard <= "0011";
+--		elsif(PRESENT_STATE=player_3_hit)then 
+--			newPlayerCard <= "0111";
+--		elsif(PRESENT_STATE=player_4_hit)then 
+--			newPlayerCard <= "1111";
+--		elsif(PRESENT_STATE=dealer_2)then
+--			newDealerCard <= "0011";
+--		elsif(PRESENT_STATE=dealer_3_hit)then
+--			newDealerCard <= "0111";
+--		elsif(PRESENT_STATE=dealer_4_hit)then
+--			newDealerCard <= "1111";
+--		elsif(PRESENT_STATE=player_win)then 
+--			greenLEDs<="11111111";
+--		elsif(PRESENT_STATE=both_bust)then
+--			greenLEDs<="11111111";
+--			redLeds<="111111111111111111";
+--		elsif(PRESENT_STATE=dealer_win)then 
+--			redLeds<="111111111111111111";
+--		elsif(PRESENT_STATE=end_game)then 
+--		else		
+--		end if;
+--	end process;
 end behAVIOURAL;
