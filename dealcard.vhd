f79dde3cc81f6ad1d16b9ed4a29ff8f6001db90a
djchung189 <dcsailing8@gmail.com>
2014-10-01 22:03:01 -0700

    Update dealcard.vhd

--- dealcard.vhd
+++ dealcard.vhd
@@ -5,35 +5,43 @@ use ieee.std_logic_unsigned.all;
 
 entity DealCard is
 	port(
-		KEY			: IN  std_logic_vector(3 downto 0); 
-		Clock_50		: IN STD_LOGIC;
-		RandomCard 	: OUT  STD_LOGIC_VECTOR(3 downto 0)
+		Deal			: IN  std_logic; 
+		Clock			: IN STD_LOGIC;
+		Reset			: IN STD_LOGIC;
+		RandomCard 		: OUT  STD_LOGIC_VECTOR(3 downto 0)
 	);
 end entity;
 
 architecture Behaviour of DealCard is
 
 signal tempcard: unsigned(3 downto 0):="0001";
+signal Randout	: std_LOGIC_VECTOR(3 downto 0) := "0000";
 
 begin
 	
-	process(clock_50)	
+	process(clock)	
 	begin
-		IF(Key(0)='1')then 
-			IF(rising_edge(clock_50)) then	
-				IF (tempcard = "1101")then
-					tempcard <= "0001";
-				else
-					tempcard <= tempcard + "0001";
-				end IF;
+		IF(rising_edge(clock)) then	
+			IF (tempcard = "1101")then
+				tempcard <= "0001";
+			else
+				tempcard <= tempcard + "0001";
 			end IF;
 		end IF;
+		
+		
 	end process;
 	
-	process(Key(0))
+	process(deal, Randout, reset)
 	begin	
-		IF(rising_edge(Key(0)))then
-			RandomCard <= std_logic_vector(tempcard);
+		IF(Deal='1' and Randout = "0000")then
+			Randout <= std_logic_vector(tempcard);
+		ELSIF( reset = '0') THEn
+			Randout <= "0000";
 		end IF;
+
 	end process;
+	
+	RandomCard <= Randout;
+	
 end Behaviour;
