78c017bd819438e73d723d9c96ef1fb292e8747c
njackien <deterex@gmail.com>
2014-10-03 19:32:46 -0700

    Update dealcard.vhd

--- dealcard.vhd
+++ dealcard.vhd
@@ -5,35 +5,49 @@ use ieee.std_logic_unsigned.all;
 
 entity DealCard is
 	port(
-		KEY			: IN  std_logic_vector(3 downto 0); 
-		Clock_50		: IN STD_LOGIC;
-		RandomCard 	: OUT  STD_LOGIC_VECTOR(3 downto 0)
+		DEAL			: IN std_logic;
+		Clock			: IN STD_LOGIC;
+		RESET			: IN STD_LOGIC;
+		RandomCard 	: OUT STD_LOGIC_VECTOR(3 downto 0)
 	);
 end entity;
 
 architecture Behaviour of DealCard is
 
-signal tempcard: unsigned(3 downto 0):="0001";
+signal tempcard: unsigned(3 downto 0):="0000";
+signal Randout	: std_LOGIC_VECTOR(3 downto 0) := "0000";
 
 begin
 	
-	process(clock_50)	
+	process(clock, reset)	
 	begin
-		IF(Key(0)='1')then 
-			IF(rising_edge(clock_50)) then	
+		if( reset = '0') then
+			tempcard <= "0000";
+		elsif(rising_edge(clock)) then	
+			IF(deal='0')then
 				IF (tempcard = "1101")then
+					tempcard <= "0001";
+				elsif( tempcard = "0000" ) then 
 					tempcard <= "0001";
 				else
 					tempcard <= tempcard + "0001";
 				end IF;
-			end IF;
+			end if;
 		end IF;
+		
+		
 	end process;
 	
-	process(Key(0))
+	process(deal, Randout, reset)
 	begin	
-		IF(rising_edge(Key(0)))then
-			RandomCard <= std_logic_vector(tempcard);
+		IF(Deal='1' and reset = '1')then
+			Randout <= std_logic_vector(tempcard);
+		ELSIF( (reset = '0')  and (Deal='1')) THEn
+			Randout <= "0000";
 		end IF;
+
 	end process;
+	
+	RandomCard <= Randout;
+	--LEDR<= std_LOGIC_VECTOR(tempcard);
 end Behaviour;
